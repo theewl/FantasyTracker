0bad5f5a990cf4f8f0bfe020da1a1269e19e2b5f
Eric Liang <liange3@uci.edu>
2019-09-12 15:12:28 -0700

    Added total average in a week

--- nbaAPI.js
+++ nbaAPI.js
@@ -15,9 +15,21 @@ var csvData = new Array();
 var firstNameArr = []
 var lastNameArr = []
 var globalCtr = 0;
-var avgPTS = [];
 var csvData2 = new Array();
 var playersTeam = []
+
+var avgPTS = [];
+var avgASTS = [];
+var avgREBS = [];
+var avgSTLS = [];
+var avgBLKS = [];
+var avg3PTM = [];
+var avgTO = [];
+var avgFGP = [];
+var avgFTP = [];
+
+var avgTotalPerWeek = 0;
+var avgTotalPerWeekArr = []
 
 
 //Used to get player's ID from local csv file
@@ -77,7 +89,7 @@ document.getElementById("addTeamBtn").addEventListener('click', () =>
     {
     //You can play with your DOM here or check URL against your regex
 	    console.log('Tab script:');
-	    console.log(document.body);
+	    //console.log(document.body);
 	    return document.body.innerHTML;
 	}
 
@@ -87,7 +99,7 @@ document.getElementById("addTeamBtn").addEventListener('click', () =>
 	    code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
 	}, (results) => 
 	{
-		console.log(results[0])
+		//console.log(results[0])
 		//Get player's name from fantasy team roster
 		var index = 0
 		//Find number of players in roster
@@ -139,6 +151,12 @@ document.getElementById("addTeamBtn").addEventListener('click', () =>
 	});
 });
 
+var storage = chrome.storage.sync;
+storage.set({name0: 'Trae Young'})
+storage.set({name1: 'Paul George'})
+storage.set({numPlayers: 2})
+
+
 //CHECK ADD TEAM BUTTON AND ADD TABLE HEADER WHENEVER A NEW WEEK IS CHOSEN
 var table = document.getElementById("tb");
 
@@ -157,6 +175,7 @@ function myFunction(){
 	    var counter2 = 0
 	    var counter3 = 0
 	    var counter4 = 0
+	    var counter5 = 0
 	    //Clears table body when new week is selected
 	    $('#tb').empty()
 
@@ -172,10 +191,10 @@ function myFunction(){
 		  var cell6 = row.insertCell(5);
 
 		  
-		  cell1.innerHTML = "ID"
+		  cell1.innerHTML = "Wait!"
 		  cell2.innerHTML = items[allKeys[rows]];
-		  cell3.innerHTML = "Traded!"
-		  cell6.innerHTML = "Team"
+		  cell3.innerHTML = "Wait!"
+		  cell6.innerHTML = "Wait!"
 		  players.push(items[allKeys[rows]])
 		}	
 
@@ -207,9 +226,20 @@ function myFunction(){
 			      // Examine the text in the response
 			      response.json().then(function(data) 
 			      {
+
 			      	    var foundID = false
 
 			      		avgPTS[data["data"]["0"]["player_id"]] = data["data"]["0"]["pts"]
+			      		avgASTS[data["data"]["0"]["player_id"]] = data["data"]["0"]["ast"]
+			      		avgREBS[data["data"]["0"]["player_id"]] = data["data"]["0"]["reb"]
+			      		avgSTLS[data["data"]["0"]["player_id"]] = data["data"]["0"]["stl"]
+			      		avgBLKS[data["data"]["0"]["player_id"]] = data["data"]["0"]["blk"]
+			      		avg3PTM[data["data"]["0"]["player_id"]] = data["data"]["0"]["fg3m"]
+			      		avgTO[data["data"]["0"]["player_id"]] = data["data"]["0"]["turnover"]
+			      		avgFGP[data["data"]["0"]["player_id"]] = data["data"]["0"]["fg_pct"]
+			      		avgFTP[data["data"]["0"]["player_id"]] = data["data"]["0"]["ft_pct"]
+
+			      		var selectedStat = document.getElementById("selectedStat").value
 
 					    //console.log(data["data"]["0"]["player_id"])
 					    while(foundID == false)
@@ -221,18 +251,112 @@ function myFunction(){
 					    	}
 						    if(data["data"]["0"]["player_id"] == document.getElementById("tb").rows[counter].cells[0].innerHTML)
 						    {
+						    	if(selectedStat == "PTS")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "P/G: " + avgPTS[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "ASTS")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "A/G: " + avgASTS[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "REBS")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "R/G: " + avgREBS[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "STLS")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "S/G: " + avgSTLS[data["data"]["0"]["player_id"]]	
+						    	else if(selectedStat == "BLKS")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "B/G: " + avgBLKS[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "3PTM")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "3PTM/G: " + avg3PTM[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "TO")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "TO/G: " + avgTO[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "FGP")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "FGP/G: " + avgFGP[data["data"]["0"]["player_id"]]
+						    	else if(selectedStat == "FTP")
+						    		document.getElementById("tb").rows[counter].cells[2].innerHTML = "FTP/G: " + avgFTP[data["data"]["0"]["player_id"]]
 
-						    	document.getElementById("tb").rows[counter].cells[2].innerHTML = "PPG: " + avgPTS[data["data"]["0"]["player_id"]]
-						    	foundID = true
+						    	avgTotalPerWeek =  (avgASTS[data["data"]["0"]["player_id"]] + avgPTS[data["data"]["0"]["player_id"]] + avgREBS[data["data"]["0"]["player_id"]]
+						    	+ avgSTLS[data["data"]["0"]["player_id"]] + avgBLKS[data["data"]["0"]["player_id"]] + avg3PTM[data["data"]["0"]["player_id"]] + data["data"]["0"]["fg3m"]
+						        + (avgFGP[data["data"]["0"]["player_id"]] * 100) 
+						        + (avgFTP[data["data"]["0"]["player_id"]] * 100)) 
+						        - avgTO[data["data"]["0"]["player_id"]] 
+
+						        avgTotalPerWeekArr[counter] = avgTotalPerWeek
+						        foundID = true
+
+
 						    }
+
 
 							counter++
 						}
 
-
 			      });
-			    }
-			  )
+			    })
+			  .then(function()
+			  {
+
+				  //Get player's team
+				  var url3 = "https://www.balldontlie.io/api/v1/players/" + result;
+				  var gamesCtr = 0
+				  var playerctr = 0
+
+				  fetch(url3)
+				  .then(
+				    function(response) 
+				    {
+
+				      if (response.status !== 200) {
+				        console.log('Looks like there was a problem. Status Code: ' +
+				          response.status);
+				        return;
+				      }
+
+				      // Examine the text in the response
+				      response.json().then(function(data) 
+				      {
+
+				      	    var foundID = false
+
+				      		playersTeam[data["id"]] = data["team"]["full_name"]
+
+						    while(foundID == false)
+						    {
+						    	if(counter3 == numOfPlayers3 + 1)
+						    	{
+						    		counter3 = 0;
+						    	}
+							    if(data["id"] == document.getElementById("tb").rows[counter3].cells[0].innerHTML)
+							    {
+
+							    	document.getElementById("tb").rows[counter3].cells[5].innerHTML = playersTeam[data["id"]]
+							    	
+							    	getTeamsPerWeek('nbaSchedule20.csv').then(function(result)
+									{
+										//MAKE DROPDOWN TO SELECT WEEKS TO SEE WEEKLY GAMES PLAYED FOR EACH PLAYER 
+										//console.log(result)
+										for(var i in result[document.getElementById("selectWeek").value]){
+											if(result[document.getElementById("selectWeek").value][i] == playersTeam[data["id"]])
+											{
+												gamesCtr++
+											}
+										}
+										document.getElementById("tb").rows[counter4].cells[3].innerHTML = (avgTotalPerWeekArr[counter5] * gamesCtr).toFixed(1)
+										document.getElementById("tb").rows[counter4].cells[4].innerHTML = gamesCtr
+										counter4++
+										counter5++
+
+									});
+
+							    	foundID = true
+							    }
+
+								counter3++
+							}
+
+
+				      });
+				    }
+				  )
+				  .catch(function(err) {
+				    console.log('Fetch Error :-S', err);
+			  	});			  
+			})
 			  .catch(function(err) {
 			    console.log('Fetch Error :-S', err);
 			  });
@@ -256,68 +380,6 @@ function myFunction(){
 			      {
 			      		//console.log(data)
 			      	    
-
-
-			      });
-			    }
-			  )
-			  .catch(function(err) {
-			    console.log('Fetch Error :-S', err);
-			  });
-
-			  //Get player's team
-			  var url3 = "https://www.balldontlie.io/api/v1/players/" + result;
-			  var gamesCtr = 0
-
-			  fetch(url3)
-			  .then(
-			    function(response) 
-			    {
-			      
-			      if (response.status !== 200) {
-			        console.log('Looks like there was a problem. Status Code: ' +
-			          response.status);
-			        return;
-			      }
-
-			      // Examine the text in the response
-			      response.json().then(function(data) 
-			      {
-			      		
-			      	    var foundID = false
-
-			      		playersTeam[data["id"]] = data["team"]["full_name"]
-
-					    while(foundID == false)
-					    {
-					    	if(counter3 == numOfPlayers3 + 1)
-					    	{
-					    		counter3 = 0;
-					    	}
-						    if(data["id"] == document.getElementById("tb").rows[counter3].cells[0].innerHTML)
-						    {
-
-						    	document.getElementById("tb").rows[counter3].cells[5].innerHTML = playersTeam[data["id"]]
-						    	
-						    	getTeamsPerWeek('nbaSchedule19.csv').then(function(result)
-								{
-									//MAKE DROPDOWN TO SELECT WEEKS TO SEE WEEKLY GAMES PLAYED FOR EACH PLAYER 
-									//console.log(result)
-									for(var i in result[document.getElementById("selectWeek").value]){
-										if(result[document.getElementById("selectWeek").value][i] == playersTeam[data["id"]])
-										{
-											gamesCtr++
-										}
-									}
-									document.getElementById("tb").rows[counter4].cells[4].innerHTML = gamesCtr
-									counter4++
-
-								});
-						    	foundID = true
-						    }
-
-							counter3++
-						}
 
 
 			      });
@@ -364,6 +426,7 @@ function getTeamsPerWeek(path)
 		var teamsPlaying = []
 		var teamsPlayingObj = {}
 		var week = 1
+		var newWeek = true
 	return new Promise(function(resolve, reject)
 	{
 		request.onload = function() 
@@ -377,20 +440,24 @@ function getTeamsPerWeek(path)
 			  csvData2.push(jsonObject[i].split(','));
 			}
 
-			currDay = csvData2[1][0].substring(8,10)
-			console.log(currDay)
+			currDay = csvData2[1][0].substring(0,3)
+			//console.log(currDay)
 
 			// Retrived data from csv file content
 			for (var i = 1; i < jsonObject.length; i++) 
 			{
 				//Check each week and add teams that are playing that week to array
-				if(currDay != csvData2[i][0].substring(8,10))
+				//console.log(currDay)
+				currDay = csvData2[i][0].substring(0,3)
+				if(currDay == 'Mon' && newWeek)
 				{
 					teamsPlayingObj["week" + week] = teamsPlaying
-					currDay = csvData2[i][0].substring(8,10)
 					teamsPlaying = []
 					week++
-
+					newWeek = false
+				}
+				if(currDay != 'Mon' && newWeek == false){
+					newWeek = true
 				}
 				teamsPlaying.push(csvData2[i][1])
 				teamsPlaying.push(csvData2[i][2])
@@ -408,8 +475,11 @@ function getTeamsPerWeek(path)
 		request.send(); 
 	});
 }
+//Initial run
+myFunction()
 //Event listener for when week dropdown
 document.getElementById("selectWeek").addEventListener("change", myFunction);
+document.getElementById("selectedStat").addEventListener("change", myFunction);
 
 
 
